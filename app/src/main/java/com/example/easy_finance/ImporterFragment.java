68af1564c9a4e8881d40c0aa2f027ca9654e6370
bleclaire6053 <71787944+bleclaire6053@users.noreply.github.com>
2021-12-01 18:30:09 -0600

    Added Buttons on the ImporterFragment to be used to go to each activity. Button functionality not yet complete.

--- app/src/main/java/com/example/easy_finance/ImporterFragment.java
+++ app/src/main/java/com/example/easy_finance/ImporterFragment.java
@@ -1,5 +1,6 @@
 package com.example.easy_finance;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 public class ImporterFragment extends Fragment {
 
@@ -17,7 +19,15 @@ public class ImporterFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_importer, container, false);
     }
+
+    private void changeActivity() {
+        //Intent intent = new Intent(ImporterFragment.this.getActivity(), ScanReceipt.class);
+        //startActivity(intent);
+    }
+
+
 }
--- app/src/main/java/com/example/easy_finance/MainActivity.java
+++ app/src/main/java/com/example/easy_finance/MainActivity.java
@@ -4,8 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
@@ -45,4 +49,6 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
     };
+
+
 }
