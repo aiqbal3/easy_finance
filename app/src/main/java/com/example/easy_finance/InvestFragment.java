119b483264e65e50fca0e47bb0f057d5f48fdfa5
Daniel de Monteiro <demonteiro@wisc.edu>
2021-12-06 17:22:03 -0600

    added graph for investment and button + dialog box for balance predictor

--- app/src/main/java/com/example/easy_finance/InvestFragment.java
+++ app/src/main/java/com/example/easy_finance/InvestFragment.java
@@ -1,15 +1,28 @@
 package com.example.easy_finance;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class InvestFragment extends Fragment {
 
@@ -27,7 +40,81 @@ public class InvestFragment extends Fragment {
         int investments = sharedPreferences.getInt("investments", 0);
 
         TextView investmentBal = (TextView) view.findViewById(R.id.investmentBal);
-        investmentBal.setText(String.valueOf(investments));
+        String investText = "$" + String.valueOf(investments);
+        investmentBal.setText(investText);
+
+        // Graph for showing past investments
+        Calendar calendar = Calendar.getInstance();
+        Date d1 = calendar.getTime();
+        calendar.add(Calendar.DATE, 1);
+        Date d2 = calendar.getTime();
+        calendar.add(Calendar.DATE, 1);
+        Date d3 = calendar.getTime();
+        calendar.add(Calendar.DATE, 1);
+        Date d4 = calendar.getTime();
+        calendar.add(Calendar.DATE, 1);
+        Date d5 = calendar.getTime();
+        calendar.add(Calendar.DATE, 1);
+        Date d6 = calendar.getTime();
+        calendar.add(Calendar.DATE, 1);
+        Date d7 = calendar.getTime();
+
+        GraphView graph = (GraphView) view.findViewById(R.id.graph);
+
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(d1, 9800),
+                new DataPoint(d2, 10000),
+                new DataPoint(d3, 10130),
+                new DataPoint(d4, 10135),
+                new DataPoint(d5, 10200),
+                new DataPoint(d6, 10301),
+                new DataPoint(d7, 10235)
+        });
+
+        graph.addSeries(series);
+
+        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(getActivity()));
+        graph.getGridLabelRenderer().setNumHorizontalLabels(3); // only 4 because of the space
+
+        graph.getViewport().setMinX(d1.getTime());
+        graph.getViewport().setMaxX(d7.getTime());
+        graph.getViewport().setXAxisBoundsManual(true);
+
+        graph.getGridLabelRenderer().setHumanRounding(false);
+
+        Button button = (Button) view.findViewById(R.id.balancePredictor);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+                builder.setMessage("This feature is a predictor and may not reflect your real future balance.");
+                builder.setTitle("Info");
+
+                //This will not allow to close dialogbox until user selects an option
+                builder.setCancelable(false);
+                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Fragment predictorFragment = new PredictorFragment();
+                        FragmentManager manager = getParentFragmentManager();
+                        //use ID container to replace fragment
+                        manager.beginTransaction().replace(R.id.container, predictorFragment).commit();
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        //  Action for 'NO' Button
+                        dialog.cancel();
+                    }
+                });
+
+                //Creating dialog box
+                AlertDialog alert = builder.create();
+                //Setting the title manually
+                //alert.setTitle("AlertDialogExample");
+                alert.show();
+            }
+        });
 
         return view;
     }
