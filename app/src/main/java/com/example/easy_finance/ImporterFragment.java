023a23536cf7aa2d917e978294eaf96811a45986
bleclaire6053 <71787944+bleclaire6053@users.noreply.github.com>
2021-12-09 13:35:11 -0600

    Added code to change from ImporterFragment to ManualFragment however it results in a crash as of now.

--- app/src/main/java/com/example/easy_finance/ImporterFragment.java
+++ app/src/main/java/com/example/easy_finance/ImporterFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,11 +25,11 @@ public class ImporterFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_importer, container, false);
     }
 
-    //add onClick listener here?
-
-    private void changeActivity() {
-        Intent intent = new Intent(ImporterFragment.this.getActivity(), Scan.class);
-        startActivity(intent);
+    //@Override
+    public void onClick() {
+        Fragment manualFragment = new ManualFragment();
+        FragmentManager manager = getParentFragmentManager();
+        manager.beginTransaction().replace(R.id.container, manualFragment).commit();
     }
 
 
